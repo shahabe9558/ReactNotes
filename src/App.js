08bc1bf6d2d6c353b0903ab081c18dbdcf701ded
Shahabenawaz Alam <shahabe9559@gmail.com>
2023-12-06 11:58:15 +0530

    form data completed

--- src/App.js
+++ src/App.js
@@ -4,38 +4,72 @@ import { useEffect, useState } from 'react';
 //learn the concept of formData and fetch all the formdata
 // in this we will handle multiple input elemet to store the form data
 function App() {
-  const [formData, setFormData] = useState({firstName:"", lastName:""});
+  const [formData, setFormData] = useState({firstName:"", lastName:"", textArea:"", isVisible:true, mode:""});
   console.log("form data is", formData);
 
   // to maintain this we use a special syntax 
   // first we copy old state and then set fucntion called and ui re-render 
   const changeHandler = (event)=> {
-  //  setFormData(event.target.value); 
+  const {value, type, checked, name} = event.target;
   setFormData((prevFormData) => {
    return{
     ...prevFormData,
-    [event.target.name] : event.target.value
+    [name] : type === "checkbox" ? checked : value
    }
   });
   }
   return (
-    <div className="App flex flex-col justify-center">
+    <div className="">
       <form>
           {/* to handle multiple input we use name attribute and creat a state object */}
+          {/* to keep state of single element we use value attribute  */}
           <input type='text' 
           placeholder='Enter Your first name'
           onChange={changeHandler}
           name='firstName'
+          value={formData.firstName}
           />
 
           <input type='text' 
           placeholder='Enter Your last name'
           onChange={changeHandler}
           name='lastName'
+          value={formData.lastName}
           />
-          <textarea placeholder='Enter Your Text' >
-
+          <textarea placeholder='Enter Your Text'
+          onChange={changeHandler}
+          name='textArea'
+          value={formData.textArea}
+          >
           </textarea>
+          <input
+          type='checkbox'
+          onChange={changeHandler}
+          name='isVisible'
+          id='isVisible'
+          checked= {formData.isVisible}
+          />
+          <label htmlFor='isVisible'>Am i visible</label>
+          <br/>
+          <br/>
+
+          <input
+          type='radio'
+          onChange={changeHandler}
+          name='mode'
+          id='mode'
+          value="Online Mode"
+          checked= {formData.mode === "Online Mode"}
+          />
+          
+          <input
+          type='radio'
+          onChange={changeHandler}
+          name='mode'
+          id='mode'
+          value="Off-Line"
+          checked = {formData.mode === "Off-Line"}
+          />
       </form>
     </div>
   );
