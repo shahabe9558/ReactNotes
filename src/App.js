7fa2b9d3a7badf6f5f6950fc12689b758ce28374
Shahabenawaz Alam <shahabe9559@gmail.com>
2023-12-18 12:22:57 +0530

    redux toolkit basic setup

--- src/App.js
+++ src/App.js
@@ -1,70 +1,24 @@
 
-import { Link, NavLink, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import Home from './components/Home';
-import About from './components/About';
-import Catalogue from './components/Catalogue';
-import Contact from './components/Contact';
-import Notfound from './components/Notfound';
-import Mainheader from './components/Mainheader';
-
+import { decrement, increment } from './redux/slices/counterSlice';
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
-    <div className="">
-      {/* Link and NavLink are differnt navlink add active class in the element  */}
-      <nav>
-        <ul>
-          <li>
-            <NavLink to={'/'}>Home</NavLink>
-          </li>
-          <li>
-            <NavLink to={'/about'}>About</NavLink>
-          </li>
-          <li>
-            <NavLink to={'/catalogue'}>Catalogue</NavLink>
-          </li>
-          <li>
-            <NavLink to={'/contact'}>Contact</NavLink>
-          </li>
-        </ul>
-      </nav>
-
-      {/* <nav>
-        <ul>
-          <li>
-            <Link to={'/'}>Home</Link>
-          </li>
-          <li>
-            <Link to={'/about'}>About</Link>
-          </li>
-          <li>
-            <Link to={'/catalogue'}>Catalogue</Link>
-          </li>
-          <li>
-            <Link to={'/contact'}>Contact</Link>
-          </li>
-        </ul>
-      </nav> */}
-      
-      {/* creating a default route parent and child relation  */}
-      <Routes>
-        <Route path='/' element = {<Mainheader/>}>
-          <Route index element = {<Home/>} />
-          <Route path='/about' element = {<About/>} />
-          <Route path='/catalogue' element = {<Catalogue/>} />
-          <Route path='/contact' element = {<Contact/>} />
-          <Route path='*' element = {<Notfound/>} />
-        </Route>
-       </Routes>
-
-      {/* this is a simple way of defining route  */}
-       {/* <Routes>
-          <Route path='/' element = {<div>This is home</div>} />
-          <Route path='/about' element = {<div>This is about</div>} />
-          <Route path='/catalogue' element = {<div>This is catalogue</div>} />
-          <Route path='/contact' element = {<div>This is contact</div>} />
-       </Routes> */}
+    <div >
+        <button onClick={() => dispatch(increment())}>
+          Increment
+        </button>
+        <br/>
+        <br/>
+        <p> {count} </p>
+        <br/>
+        <br/>
+        <button onClick={()=> dispatch(decrement())}>
+          Decrement
+        </button>
     </div>
-  );
+  )
 }
 export default App;
